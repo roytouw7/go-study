3d3ad15eea087a520c6355a1f6a49e37d3dcfef6
Roy Touw <roytouw7@gmail.com>
2023-10-23 21:35:56 +0200

    added unit test of real life example.

--- ErrorHandling/Monad/ErrorMonad.go
+++ ErrorHandling/Monad/ErrorMonad.go
@@ -3,35 +3,35 @@ package Monad
 
 type unaryThrower[T, U any] func(input T) (U, error)
 
-type withError[T any] struct {
+type WithError[T any] struct {
 	Value T
 	Err   error
 }
 
 // Wrap convert generic value to a wrapped version usable in the monad
-func Wrap[T any](value T) withError[T] {
-	return withError[T]{Value: value, Err: nil}
+func Wrap[T any](value T) WithError[T] {
+	return WithError[T]{Value: value, Err: nil}
 }
 
 // Unwrap unwraps the wrapped value to the value and error
-func Unwrap[T any](we withError[T]) (interface{}, error) {
+func Unwrap[T any](we WithError[T]) (interface{}, error) {
 	return we.Value, we.Err
 }
 
 // Curry transform a binary function to a curried unary function (T, U) => (V, error) into (T) => (U) => (V, error)
 // allows for conveniently using binary functions in the error monad
-func Curry[T, U, V any](fn func(i T, j U) (V, error), i T) func(i U) (V, error) {
+func Curry[T, U, V any](i T, fn func(i T, j U) (V, error)) func(i U) (V, error) {
 	return func(j U) (V, error) {
 		return fn(i, j)
 	}
 }
 
 // Bind operates fn on inner value of wrapped input if error equals nil, else return err and default value for U
-func Bind[T, U any](we withError[T], fn unaryThrower[T, U]) withError[U] {
+func Bind[T, U any](we WithError[T], fn unaryThrower[T, U]) WithError[U] {
 	if we.Err != nil {
-		return withError[U]{Err: we.Err}
+		return WithError[U]{Err: we.Err}
 	}
 
 	value, newErr := fn(we.Value)
-	return withError[U]{Value: value, Err: newErr}
+	return WithError[U]{Value: value, Err: newErr}
 }
--- ErrorHandling/Monad/ErrorMonad_test.go
+++ ErrorHandling/Monad/ErrorMonad_test.go
@@ -1,6 +1,7 @@
 package Monad
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
@@ -82,7 +83,7 @@ func (test *TestSuite) TestMonad_BivariateFunction() {
 
 	a := Bind(Wrap(1), identity)
 	b := Bind(a, sum(2))
-	c := Bind(b, Curry(sumNormal, 2))
+	c := Bind(b, Curry(2, sumNormal))
 
 	value, err := Unwrap(c)
 	assert.Nil(test.T(), err)
@@ -117,39 +118,65 @@ func (test *TestSuite) TestMonad_ComplexType() {
 	assert.Equal(test.T(), "Roy", value)
 }
 
-// todo make a proper example using methods, chain of some methods where normally you would have error handling in between
-// todo is there ever a case for nullary functions where a monad is nice? not really? you could just wrap with the error en join the monad from there? make unit test to proof this works
+type HotelService struct{}
 
-//type person struct {
-//	name string
-//	age  int
-//}
-//
-//type exam struct {
-//	p     person
-//	score int
-//}
-//
-//func (t exam) setScore(p person) (exam, error) {
-//	return exam{
-//		p:     p,
-//		score: 0,
-//	}, nil
-//}
-//
-//func (t exam) getScore(p person)
-//
-//func (test *TestSuite) TestMonad_Method() {
-//	p := person{
-//		name: "Roy",
-//		age:  30,
-//	}
-//
-//	t := exam{}
-//
-//	a := Bind(Wrap(p), t.setScore)
-//
-//	value, err := Unwrap(b)
-//	assert.Nil(test.T(), err)
-//	assert.Equal(test.T(), "Roy", value)
-//}
+type Hotel struct {
+	uuid string
+}
+
+type Policies struct {
+	policy string
+}
+
+type WebPages struct {
+	page string
+}
+
+func (h *HotelService) GetHotel(ctx context.Context, uuid string) (Hotel, error) {
+	return Hotel{uuid: uuid}, nil
+}
+func (h *HotelService) GetGuaranteePolicies(ctx context.Context, hotel Hotel) (Policies, error) {
+	return Policies{policy: fmt.Sprintf("Policy for hotel %s", hotel.uuid)}, nil
+}
+func (h *HotelService) GetWebpagesForHotel(ctx context.Context, hotel Hotel) (WebPages, error) {
+	return WebPages{page: fmt.Sprintf("Page for hotel %s", hotel.uuid)}, nil
+}
+
+var hotelService HotelService
+
+// TestMonad_RealLifeExample_WithoutMonad tests the real-life example without a monad, as found in real code
+func (test *TestSuite) TestMonad_RealLifeExample_WithoutMonad() {
+	hotel, err := hotelService.GetHotel(nil, "1337")
+	if err != nil {
+		return // return error
+	}
+
+	policies, err := hotelService.GetGuaranteePolicies(nil, hotel)
+	if err != nil {
+		return // return error
+	}
+
+	webpages, err := hotelService.GetWebpagesForHotel(nil, hotel)
+	if err != nil {
+		return // return error
+	}
+
+	test.Equal(policies.policy, fmt.Sprintf("Policy for hotel %s", hotel.uuid))
+	test.Equal(webpages.page, fmt.Sprintf("Page for hotel %s", hotel.uuid))
+}
+
+// TestMonad_RealLifeExample_WithMonad tests the real-life example using a monad
+func (test *TestSuite) TestMonad_RealLifeExample_WithMonad() {
+	hotel := Bind(Wrap("1337"), Curry(nil, hotelService.GetHotel))
+	policies := Bind(hotel, Curry(nil, hotelService.GetGuaranteePolicies))
+	webpages := Bind(hotel, Curry(nil, hotelService.GetWebpagesForHotel))
+
+	if webpages.Err != nil { // todo not a fan of this, appears we only check webpages error now
+		return // return error	// benefit might be bigger if the error handling is more than just a return err
+	}
+
+	test.Equal(policies.Value.policy, fmt.Sprintf("Policy for hotel %s", hotel.Value.uuid))
+	test.Equal(webpages.Value.page, fmt.Sprintf("Page for hotel %s", hotel.Value.uuid))
+}
+
+// todo is there ever a case for nullary functions where a monad is nice? not really? you could just wrap with the error en join the monad from there? make unit test to proof this works
